c7da60560de2a62e6667ab962d98768664c5a4e8
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-11-22 16:59:08 +0100

    feat: execute install:api command

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\BannedPokemonController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('banned')->name('banned.')->group(function () {
+    Route::get('/', [BannedPokemonController::class, 'index'])->name('index');
+    Route::post('/store', [BannedPokemonController::class, 'store'])->name('store');
+    Route::delete('/destroy/{bannedPokemon}', [BannedPokemonController::class, 'destroy'])->name('destroy');
+});
