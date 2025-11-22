e20dd6b92c11bf2fde3c32754edfa60a53133e6e
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-11-22 18:16:20 +0100

    feat: create pokemons table migration

---
+++ database/migrations/2025_11_22_161512_create_pokemon_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pokeapi_id')->unique();
+            $table->string('name', 255)->unique();
+            $table->json('pokeapi_data');
+            $table->timestamp('cached_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
